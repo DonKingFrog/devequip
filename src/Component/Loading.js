a967f52f625fb110681a7b8793e6b4e60b885849
DonKingFrog <don.tgh1@gmail.com>
2025-03-01 16:49:40 +0200

    e

--- src/Component/Loading.js
+++ src/Component/Loading.js
@@ -2,28 +2,8 @@ import { useEffect } from "react";
 
 function Loading() {
     useEffect(() => {
-        const handleLoad = () => {
-            const preloader = document.querySelector("#preloader")
-            if (preloader) { preloader.classList.add("loaded") };
-        };
-
-        function isMobileDevice() {
-            return /Mobi|Android/i.test(navigator.userAgent);
-        }
-        
-        if (isMobileDevice()) {
-            setTimeout(() => {
-                handleLoad()
-            }, 500);
-        } else {
-            window.addEventListener("load", handleLoad);
-        }
-
-        return () => {
-            if (!isMobileDevice()) {
-                window.removeEventListener("load", handleLoad);
-            }
-        };
+        const preloader = document.querySelector("#preloader")
+        if (preloader) { preloader.classList.add("loaded") };
     }, [])
     return (
         <div id="preloader"></div>
