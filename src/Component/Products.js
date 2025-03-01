a3927e35302d33560a1fd3490a7f0d19e98247d1
DonKingFrog <don.tgh1@gmail.com>
2025-03-01 20:57:49 +0200

    e

--- src/Component/Products.js
+++ src/Component/Products.js
@@ -36,6 +36,27 @@ function RenderProductPage() {
         loadPost();
     }, [productId]);
 
+    useEffect(() => {
+        document.title = `Dev Equip - ${jsonContent.name}`;
+        document.head.innerHTML += `
+          <meta name="theme-color" content="#000a13" />
+          <meta name="og:site_name" content="Dev Equip" />
+          <meta name="og:determiner" content="Be Equipped Today" />
+          <meta property="og:title" content="Dev Equip" />
+          <meta property="og:description" content="Dev Equip is a Roblox group focused on providing developers with tools, resources, and a community to help create amazing games/tech." />
+          <meta property="og:logo" content="/logo.png" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="200" />
+          <meta property="og:image:height" content="200" />
+          <meta property="og:image" content="/embedbackground.png" />
+          <meta property="og:url" content="https://devequip.com/home" />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@donkingfrog" />
+          <meta name="twitter:creator" content="@donkingfrog" />
+        `;
+    }, []);
+
     if (isLoading) {
         return <div></div>;
     }
