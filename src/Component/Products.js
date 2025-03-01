74aa21741ce4ec274d82360e307fa09f4542ad43
DonKingFrog <don.tgh1@gmail.com>
2025-03-01 22:39:40 +0200

    e

--- src/Component/Products.js
+++ src/Component/Products.js
@@ -49,24 +49,6 @@ function RenderProductPage() {
 
     return (
         <>
-            <Helmet>
-                <title>{jsonContent.name}</title>
-                <meta name="title" content={jsonContent.name} />
-                <meta name="theme-color" content="#000a13" />
-                <meta name="og:site_name" content="Dev Equip" />
-                <meta name="og:determiner" content="Be Equipped Today" />
-                <meta property="og:title" content={jsonContent.name} />
-                <meta property="og:description" content={jsonContent.description || "Dev Equip is a Roblox group focused on providing developers with tools, resources, and a community to help create amazing games/tech."} />
-                <meta property="og:logo" content="/logo.png" />
-                <meta property="og:image:type" content="image/png" />
-                <meta property="og:image:width" content="200" />
-                <meta property="og:image:height" content="200" />
-                <meta property="og:image" content={jsonContent.logo} />
-                <meta property="og:url" content={jsonContent.redirect} />
-                <meta property="og:type" content="website" />
-                <meta name="twitter:site" content="@donkingfrog" />
-                <meta name="twitter:creator" content="@donkingfrog" />
-            </Helmet>
             
             <button className="page-product-preview" onClick={(event) => (document.querySelector(".page-product-preview").classList.remove("active"))}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
