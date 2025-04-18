7d1ba5467af857ffb9e9929475412b8c0403295d
DonKingFrog <don.tgh1@gmail.com>
2025-04-18 11:36:01 +0200

    e

--- src/Pages/Docs.js
+++ src/Pages/Docs.js
@@ -253,7 +253,7 @@ function RenderDocumentation() {
                     </div>
                 </div>
 
-                {/* <div className="dropdown-wrapper active">
+                <div className="dropdown-wrapper active">
                     <button id="top" onClick={(event) => { toggleDropdown(event)} }>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
                         Nexus Dynamics
@@ -262,7 +262,7 @@ function RenderDocumentation() {
                     <div id="content">
                         <A href="/docs/nexusdynamics/installation/">Installation</A>
                     </div>
-                </div> */}
+                </div>
 
                 {/* <div className="dropdown-wrapper active">
                     <button id="top" onClick={(event) => { toggleDropdown(event)} }>
